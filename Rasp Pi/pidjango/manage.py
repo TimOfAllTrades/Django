8066b4e578de03ec2d19bec2609b38de625eb190
Tim <xin2theblue@gmail.com>
2023-10-31 17:08:56 +0900

    added raspberry pi

--- Rasp Pi/pidjango/manage.py
+++ Rasp Pi/pidjango/manage.py
@@ -6,10 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-
-    # This imports the webserver settings.
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoproj.settings')
-
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pidjango.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
