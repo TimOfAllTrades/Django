0f1d4a21b0af89e988d0f8f233d87df457483291
Tim Yang <Xin2theblue@gmail.com>
2023-12-29 14:36:08 +0900

    added in users.config to settings

--- 06 Starting/django_project/django_project/settings.py
+++ 06 Starting/django_project/django_project/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
-    'users.apps.UsersConfig',
+    'users.apps.UsersConfig', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
