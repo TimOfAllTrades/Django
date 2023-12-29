bb2b6f1d7ea301461f4f273384e0e5b74db4e9e7
Tim Yang <Xin2theblue@gmail.com>
2023-12-29 14:43:02 +0900

    added user creation form

--- 06 Starting/django_project/users/views.py
+++ 06 Starting/django_project/users/views.py
@@ -3,3 +3,5 @@ from django.contrib.auth.forms import UnderCreationForm
 
 # Create your views here.
 def register(request):
+    form = UserCreationForm()
+    return render(request, 'users/register.html', {'form' : form})
