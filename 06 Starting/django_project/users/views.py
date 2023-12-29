5e9c82cf80c1c306f82bf7cb8235d9177458b56e
Tim Yang <Xin2theblue@gmail.com>
2023-12-29 15:05:55 +0900

    added some notes

--- 06 Starting/django_project/users/views.py
+++ 06 Starting/django_project/users/views.py
@@ -3,5 +3,5 @@ from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
+    form = UserCreationForm()  #This is what creates all the extra password rules and text
     return render(request, 'users/register.html', {'form' : form})
